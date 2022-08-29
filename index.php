5d3872ab11e01bbb8278505c003b3bcbe0ca7b66
Debian <debian@debian10.linuxvmimages.local>
2022-08-28 23:53:24 -0500

    Changes to be committed: 	modified:   index.php

--- index.php
+++ index.php
@@ -5,7 +5,11 @@
 <h1>My first PHP page</h1>
 
 <?php
-echo "Hello World! hello";
+echo "Hello World! hello hello hello hello hello";
+$color = "red";
+echo "My car is " . $color . "<br>";
+echo "My house is " . $COLOR . "<br>";
+echo "My boat is " . $coLOR . "<br>";
 ?>
 
 </body>
