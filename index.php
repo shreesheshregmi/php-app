a9775a9c65bd6a7bef7a37056619cd48adc2b272
Debian <debian@debian10.linuxvmimages.local>
2022-08-29 01:45:28 -0500

    made  changes to index file

--- index.php
+++ index.php
@@ -5,7 +5,7 @@
 <h1>My first PHP page</h1>
 
 <?php
-echo "bird man, bird man, bird man"
+echo "bird man, bird man, bird man, HELLO, Hello"
 echo "hola, hola, Hello World! hello hello hello hello hello. hello sunshine, paradise";
 $color = "red";
 echo "My car is " . $color . "<br>";
