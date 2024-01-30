fc8cc7d6bbe89b62749f44b4f906ef412f5e3dd4
Your Name <you@example.com>
2024-01-30 21:25:00 +0545

    changes index.html

--- index.php
+++ index.php
@@ -6,7 +6,7 @@
 
 <?php
 echo "bird man,hola, hi there, bird man, bird man, HELLO, Hello";
-echo "hola, hola, Hello World! hello hello hello hello hello. hello sunshine, paradise";
+echo "hola, hola, Hello World! hello hello hello hello hello. hello sunshine, paradise, sun";
 $color = "black";
 echo "My car is " . $color . "<br>";
 echo "My house is " . $COLOR . "<br>";
