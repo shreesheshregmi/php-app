96b3a3492ce4019632caa80e5f04d63529208efc
Your Name <you@example.com>
2024-02-05 21:50:39 +0545

    changed color

--- index.php
+++ index.php
@@ -7,7 +7,7 @@
 <?php
 echo "bird man,hola, hi there, bird man, bird man, HELLO, Hello";
 echo "hola, hola, Hello World! hello hello hello hello hello. hello sunshine, paradise, sun";
-$color = "black";
+$color = "blue";
 echo "My car is " . $color . "<br>";
 echo "My house is " . $COLOR . "<br>";
 echo "My boat is " . $coLOR . "<br>";
